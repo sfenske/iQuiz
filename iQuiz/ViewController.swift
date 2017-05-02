3b37f5f23b3a8b33636c1b11f7fcb3e79b5bb475
Sydnie Fenske <sydniefenske@d-173-250-247-128.dhcp4.washington.edu>
2017-05-02 10:52:04 -0700

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Sydnie Fenske on 5/2/17.
+//  Copyright © 2017 Sydnie Fenske. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
